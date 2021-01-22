5a0b76393616bef0af2458ac41c1ebf47b7a0d92
Nwon2464 <dnjsskarb7@gmail.com>
2021-01-22 23:50:32 +0900

    3d-carousel

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("H");
+const carousel = document.querySelector(".carousel");
+const cellCount = 8;
+let selectedIndex = 0;
+
+function rotateCarousel() {
+  let angle = (selectedIndex / cellCount) * -360;
+  console.log(angle, selectedIndex, cellCount);
+  carousel.style.transform = "translateZ(-253px) rotateY(" + angle + "deg)";
+}
+const prevButton = document.querySelector(".previous-button");
+prevButton.addEventListener("click", function () {
+  selectedIndex--;
+  rotateCarousel();
+});
+
+const nextButton = document.querySelector(".next-button");
+nextButton.addEventListener("click", function () {
+  selectedIndex++;
+  rotateCarousel();
+});
